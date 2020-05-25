abd6891058e8333b82d990d6e9822696da123899
yerim.ju <yerim.ju@lge.com>
2020-05-25 14:14:23 +0900

    implement setting username and video attachment feature

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const OWNER_PASSWD = '***';
 router.post("/nodemailerTest", function (req, res, next) {
     // console.log(req.body);
     let email = req.body.email;
+    let username = req.body.name;
 
     let transporter = nodemailer.createTransport({
         // enter your gmail account, check security option
@@ -23,7 +24,30 @@ router.post("/nodemailerTest", function (req, res, next) {
         from: OWNER_EMAIL,
         to: email,
         subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
+        // text: 'Hello, ' + username,
+        html: `<p>Hello, ` + username + `!</p>
+        <p>Here's a video for you as an embedded attachment:<img src="cid:note@example.com"/></p>
+        <video src="cid:nyan@example.com" controls preload autoplay></video>`,
+        attachments: [
+            // Binary Buffer attachment
+            {
+                filename: 'image.png',
+                content: Buffer.from(
+                    'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
+                    '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
+                    'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC',
+                    'base64'
+                ),
+
+                cid: 'note@example.com' // should be as unique as possible
+            },
+            // File Stream attachment
+            {
+                filename: 'test.mp4',
+                path: 'http://127.0.0.1:3000/static/topgolf_ad_360p.mp4',
+                cid: 'nyan@example.com' // should be as unique as possible
+            }
+        ]
     };
     console.log(mailOptions);
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const fs = require('fs');
 
 const port = 3000;
+var USER_NAME = 'Yerim';
 
 
 // set body parser
@@ -15,12 +17,20 @@ app.use(bodyParser.json());
 // set router
 app.use('/', require('./routes'));
 
+// set static files
+app.use('/static', express.static(__dirname + '/public'));
+
 // set ejs file as view
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/', function (req, res) {
     res.render('view');
+});
+
+app.get('/upload', function (req, res) {
+    // send username
+    res.render('video', {username: USER_NAME});
 });
 
 app.listen(port, function () {
